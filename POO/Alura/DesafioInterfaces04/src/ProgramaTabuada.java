d9a98530accacbab6241eebf46c5c9fed08777ac
Daniel <dangosil@yahoo.com.br>
2026-03-09 00:34:47 -0300

    feat(tabuada): permitir que o user escolha quall tabuada quer ver

--- POO/Alura/DesafioInterfaces04/src/ProgramaTabuada.java
+++ POO/Alura/DesafioInterfaces04/src/ProgramaTabuada.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class ProgramaTabuada {
     static void main(String[] args) {
         TabuadaMulplicacao tabuadaMult = new TabuadaMulplicacao();
+        Scanner input = new Scanner(System.in);
 
-        tabuadaMult.mostrarTabuada(19);
+        System.out.println("Digite qual tabuada de multiplicação você quer ver: ");
+        int n = input.nextInt();
+        tabuadaMult.mostrarTabuada(n);
     }
 }
